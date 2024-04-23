cebafe39e5a5e44481b964b14e2991a39c3dac9b
unknown <Quadrant@QHYDL1706.quadranttechnologies.com>
2024-04-23 14:17:18 +0530

    new changes

---
+++ DEO.java
@@ -0,0 +1,18 @@
+package new_practice;
+
+public class DEO extends Teacher{
+
+	
+	
+	public static DEO result;
+
+	public void Result() {
+		System.out.println("Result Details");
+	
+	}
+
+	
+	
+}
+
+
